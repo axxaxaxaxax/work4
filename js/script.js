cbcaf61f0ea122abbc5b4a76719569f5ee78c5b1
axxaxaxaxax <167338615+axxaxaxaxax@users.noreply.github.com>
2024-06-22 09:34:18 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,172 @@
+//#1
+function fun1(){
+    $(".header_item_first").css("background", "#8e0000")
+    $(".header_item_second").css("background", "rgb(232, 232, 232)")
+    $(".header_item_third").css("background", "rgb(232, 232, 232)")
+
+    $(".header_text_first").css("color", "white")
+    $(".header_text_second").css("color", "black")
+    $(".header_text_third").css("color", "black")
+
+    $(".header_image").css("background", "url(file:///C:/Users/bilan/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/jquery%20%D0%B7%D0%B0%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5/img/pulloverJuzo.png)")
+    $(".header_image").css("background-size", "100% 100%")
+    $(".header_image").css("width", "550px")
+    $(".header_image").css("margin", "60px 80px 0 0")
+    
+}
+function fun2(){
+    $(".header_item_first").css("background", "rgb(232, 232, 232)")
+    $(".header_item_second").css("background", "#8e0000")
+    $(".header_item_third").css("background", "rgb(232, 232, 232)")
+
+    $(".header_text_first").css("color", "black")
+    $(".header_text_second").css("color", "white")
+    $(".header_text_third").css("color", "black")
+
+    $(".header_image").css("background", "url(file:///C:/Users/bilan/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/jquery%20%D0%B7%D0%B0%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5/img/longWhatDidAll.png)")
+    $(".header_image").css("background-size", "100% 100%")
+    $(".header_image").css("width", "450px")
+    $(".header_image").css("margin", "90px 0 0 50px")
+    
+}
+function fun3(){
+    $(".header_item_first").css("background", "rgb(232, 232, 232)")
+    $(".header_item_second").css("background", "rgb(232, 232, 232)")
+    $(".header_item_third").css("background", "#8e0000")
+
+    $(".header_text_first").css("color", "black")
+    $(".header_text_second").css("color", "black")
+    $(".header_text_third").css("color", "white")
+
+    $(".header_image").css("background", "url(file:///C:/Users/bilan/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/jquery%20%D0%B7%D0%B0%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5/img/longImReal.png)")
+    $(".header_image").css("background-size", "100% 100%")
+    $(".header_image").css("width", "400px")
+    $(".header_image").css("margin", "90px 0 0 50px")
+    
+}
+
+//#2
+let count1 = 0
+let count2 = 0
+let count3 = 0
+
+function fun4(){
+    count1++
+    $(".first_count").text(count1)
+}
+function fun5(){
+    count2++
+    $(".second_count").text(count2)
+}
+function fun6(){
+    count3++
+    $(".third_count").text(count3)
+}
+
+//#3
+function fun7(){
+    $(".details1").slideDown(700)
+    $(".details2").slideUp(700)
+    $(".details3").slideUp(700)
+    $(".details4").slideUp(700)
+
+    $(".shipping_image").css("background", "url(file:///C:/Users/bilan/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/jquery%20%D0%B7%D0%B0%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5/img/pulloverMaybe.png)")
+    $(".shipping_image").css("background-size", "100% 100%")
+    $(".shipping_image").css("width", "37%")
+}
+function fun8(){
+    $(".details1").slideUp(700)
+    $(".details2").slideDown(700)
+    $(".details3").slideUp(700)
+    $(".details4").slideUp(700)
+
+    $(".shipping_image").css("background", "url(file:///C:/Users/bilan/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/jquery%20%D0%B7%D0%B0%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5/img/schalRbw.jpg)")
+    $(".shipping_image").css("background-size", "100% 100%")
+    $(".shipping_image").css("width", "37%")
+}
+function fun9(){
+    $(".details1").slideUp(700)
+    $(".details2").slideUp(700)
+    $(".details3").slideDown(700)
+    $(".details4").slideUp(700)
+
+    $(".shipping_image").css("background", "url(file:///C:/Users/bilan/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/jquery%20%D0%B7%D0%B0%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5/img/pulloverI.png)")
+    $(".shipping_image").css("background-size", "100% 100%")
+    $(".shipping_image").css("width", "37%")
+}
+function fun10(){
+    $(".details1").slideUp(700)
+    $(".details2").slideUp(700)
+    $(".details3").slideUp(700)
+    $(".details4").slideDown(700)
+
+    $(".shipping_image").css("background", "url(file:///C:/Users/bilan/OneDrive/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/jquery%20%D0%B7%D0%B0%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5/img/compTeppichWimm.png)")
+    $(".shipping_image").css("background-size", "100% 100%")
+    $(".shipping_image").css("width", "45%")
+}
+
+
+
+$('#logoButton').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $(".goods").offset().top
+    }, 1000);
+  });
+
+
+
+
+
+
+  $(function() {
+    $.fn.scrollToTop = function() {
+     $(this).hide().removeAttr("href");
+     if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+     var scrollDiv = $(this);
+     $(window).scroll(function() {
+      if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+      else $(scrollDiv).fadeIn("slow")
+     });
+     $(this).click(function() {
+         $("html, body").animate({scrollTop: 0}, 1500)
+     })
+    }
+   });
+   $(function() {
+    $("#go-top").scrollToTop();
+   });
+
+
+
+
+
+
+//    var block_show = false;
+ 
+//    function scrollTracking(){
+//        if (block_show) {
+//            return false;
+//        }
+    
+//        var wt = $(window).scrollTop();
+//        var wh = $(window).height();
+//        var et = $('.goods').offset().top;
+//        var eh = $('.goods').outerHeight();
+//        var dh = $(document).height();   
+    
+//        if (wt + wh >= et || wh + wt == dh || eh + et < wh){
+//            block_show = true;
+           
+//            $('.goods .cart:eq(0)').show('fast', function(){
+//                $(this).next().show('fast', arguments.callee);
+//            });
+//        }
+//    }
+    
+//    $(window).scroll(function(){
+//        scrollTracking();
+//    });
+       
+//    $(document).ready(function(){ 
+//        scrollTracking();
+//    });
